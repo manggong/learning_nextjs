a4fd5303480822fbace73464c01331228ba9e5c0
manggong <jihwank1995@gmail.com>
2021-04-11 22:06:43 +0900

    add getStaticProps

--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -1,32 +1,19 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+import Link from 'next/link';
 
 const App = () => {
-  const [name, setName] = useState('');
-  const router = useRouter();
-
-  const toTomato = () => {
-    router.push('/tomato');
-  };
-
-  const onChange = (e) => {
-    setName(e.target.value);
-  };
-
-  const toInputName = () => {
-    router.push(`/vegetable/${name}`);
-  };
+  const [username, setUsername] = useState('');
 
   return (
     <div>
-      <button type='button' onClick={toTomato}>
-        going to tomato
-      </button>
-      <p>name</p>
-      <input value={name} onChange={onChange} style={{ marginRight: '12px' }} />
-      <button type='button' onClick={toInputName}>
-        going to inputValue
-      </button>
+      <label>
+        username
+        <input value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <p>searching github</p>
+      <Link href={`/users/${username}`}>
+        <a>{`searching ${username}`}</a>
+      </Link>
     </div>
   );
 };
