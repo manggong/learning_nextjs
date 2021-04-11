d8ca5d904876bad80c662449b757dd3660fdf6cf
manggong <jihwank1995@gmail.com>
2021-04-11 22:29:43 +0900

    apply getInitialProps

--- next-app/pages/users/[name].js
+++ next-app/pages/users/[name].js
@@ -5,19 +5,34 @@ const name = ({ user }) => {
   return <div>{username}</div>;
 };
 
-export const getServerSideProps = async ({ query }) => {
+name.getInitialProps = async ({ query }) => {
   const { name } = query;
   try {
     const res = await fetch(`https://api.github.com/users/${name}`);
     if (res.status === 200) {
       const user = await res.json();
-      return { props: { user } };
+      return { user };
     }
-    return { props: {} };
+    return {};
   } catch (e) {
     console.log(e);
-    return { props: {} };
+    return {};
   }
 };
+
+// export const getServerSideProps = async ({ query }) => {
+// const { name } = query;
+// try {
+//   const res = await fetch(`https://api.github.com/users/${name}`);
+//   if (res.status === 200) {
+//     const user = await res.json();
+//     return { props: { user } };
+//   }
+//   return { props: {} };
+// } catch (e) {
+//   console.log(e);
+//   return { props: {} };
+// }
+// };
 
 export default name;
